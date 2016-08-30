bf8400d9c33ad0013f57eaebd08d3bc2a085a710
TaofEn <463988629@qq.com>
2016-08-30 12:29:40 +0800

    CNAME、scrolltopJs

--- js/index.js
+++ js/index.js
@@ -3,5 +3,12 @@
 
     $('.parallax').parallax();
 
+    $('a').click(function() {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
